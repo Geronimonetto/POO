947adf9dd9ccb5bfd55c237c9c7bf1bd3fae15c1
Geronimo Morais <108224476+Geronimonetto@users.noreply.github.com>
2022-10-25 20:57:17 -0300

    Delete PycharmProjects/pythonProject/projeto_git directory

--- PycharmProjects/pythonProject/projeto_git/main.py
+++
@@ -1,6 +0,0 @@
-from Classes import dinheiro
-
-if __name__ == '__main__':
-    inicio = dinheiro()
-    inicio.menu()
-
--- PycharmProjects/pythonProject/projeto_git/valores.py
+++
@@ -1,5 +0,0 @@
-todas = {'US$ (Dólar)': {'País': 'Estados Unidos', 'dinheiro': 5.33},
-         '£ (libra)': {'País': 'Reino Unido', 'dinheiro': 5.95},
-         '¥ (Iene)': {'País': 'Japão', 'dinheiro': 0.036},
-         'R (rand)': {'País': 'África', 'dinheiro': 0.29}}
-
