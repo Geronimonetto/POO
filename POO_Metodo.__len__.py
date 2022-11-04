1602f677cf4870ad74026e153e0d422aa4654d90
geronimo morais <geronimomorais@hotmail.com>
2022-11-04 19:48:16 -0300

    Enviando modificação

--- POO_Metodo.__len__.py
+++ POO_Metodo.__len__.py
@@ -1,20 +1,19 @@
 #Sempre que usamos a função len com uma classe para fazer a contagem
 # é como se tivessemos usando o método mágico __len__.
 
-class Teste:
+class Teste():
 
-    def __init__(self):
+    def __init__(self,nome):
         pass
 
-    
     def __len__(self):
-        return 55
+        pass
     
 
-a = Teste('nome',12,132)
+a = Teste("nome")
 
 #O valor do __len__ vai ser o valor que estiver dentro do método, porém é usado
 #quando temos objetos dentro desse objeto a como por exemplo, listas, mostrando o valor
 #de quantos objetos tem dentro da classe;
 
-print(len(a))
+print(a.__len__())
