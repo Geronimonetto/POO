6232688da6c26abe546b32d848f35b42c29c5a6c
geronimo morais <geronimomorais@hotmail.com>
2022-11-05 20:40:50 -0300

    Enviando Explicação

--- POO_Explicacao_Herenca.py
+++ POO_Explicacao_Herenca.py
@@ -1,3 +1,19 @@
+"""
+Quero criar um Animal
+
+Animal - uma classe ( podendo ser abstrata, a base) -> metodo respirar (é um animal)
+	
+	- Do animal quero criar um lobo(herdando de animal) - tudo que tem em animal tem em lobo - logo também herda o metodo respirar, se criar uivar aqui na classe lobo, a classe 
+    animal não tem o metodo uivar. ( lobo é um lobo e também é um animal)
+	
+	- Quero criar mais específico cachorro( herda de lobo) - cachorro também recebe respirar - cachorro também 
+    tem o metodo uivar, criado metodo cachorro aqui dentro nenhuma das classes acima recebe o metodo latir.(cachorro é um cachorro, um lobo e um animal.)
+
+
+
+"""
+
+
 #Bibliotecas
 class Bibliotecas:  #Classe Pai
     def chama_metodo_interface(self):
